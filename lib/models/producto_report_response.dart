bf8ebcd377864a57863f3c5b99b4213b0575116c
isabelOz <74585357@certus.edu.pe>
2022-07-15 20:08:55 -0500

    committ

---
+++ lib/models/producto_report_response.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:OTAKUMON/models/producto_models/producto_rport.dart';
+class ProductoReportResponse {
+    ProductoReportResponse({
+        this.productoReport,
+    });
+
+    List<ProductoReport> productoReport;
+
+    factory ProductoReportResponse.fromJson(String str) => ProductoReportResponse.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory ProductoReportResponse.fromMap(Map<String, dynamic> json) => ProductoReportResponse(
+        productoReport: List<ProductoReport>.from(json["productoReport"].map((x) => ProductoReport.fromMap(x))),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "productoReport": List<dynamic>.from(productoReport.map((x) => x.toMap())),
+    };
+}
